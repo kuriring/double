12c0aaaed417e6b8e1b3eb9c62e81914bb1cc4d2
kuriring <kuriringjy@gmail.com>
2017-03-05 21:23:51 +0900

    2017_03_05 div_mul+

---
+++ level2/div_mul/Readme.md
@@ -0,0 +1,2 @@
+#Problem Link
+- http://14.45.117.93/30stair/div_mul/div_mul.php?pname=div_mul
---
+++ level2/div_mul/div_mul.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(int argc,char* argv[]){
+    int m,n;
+    scanf("%d %d",&m,&n);
+    if(m==0)    
+        printf("%d !| %d",m,n);
+    else if(n%m==0)
+        printf("%d | %d",m,n);
+    else
+        printf("%d !| %d",m,n);
+    return 0;
+}
